7bbb42844ce8b9e97de29583611a688c2fb9e9c9
chirag <chiragbfreelancer@gmail.com>
2019-02-25 18:27:21 +0530

    Added JSDOC documentation for donationManager

--- chaincode/donation_manager_chaincode/node/donationManager.js
+++ chaincode/donation_manager_chaincode/node/donationManager.js
@@ -4,17 +4,31 @@ const util = require("util");
 const Validations = require("./services/validations");
 const Helpers = require("./services/helpers");
 
+/**
+ * Provides functionality for DonationManager chaincode
+ *
+ * @class
+ * @requires Validations
+ * @requires Helpers
+ * @requires shim
+ * @requires util
+ */
 class DonationManager {
   /**
-   * @dev Initialize chaincode: DonationManager
+   * Initialize chaincode: DonationManager
+   *
+   * @constructor Init
+   * @param {Object} stub - encapsulates the APIs between the chaincode implementation and the Fabric peer
+   * @throws Will throw if number of input arguments is not zero
+   * @returns {success} success if Init successful
    */
-  async Init(stub) {
+  Init(stub) {
     console.info("========= Donation Manager Init =========");
     const ret = stub.getFunctionAndParameters();
     const args = ret.params;
 
     try {
-      Validations.checkArgsLength(args, 0);
+      Validations.checkLength(args, 0);
       return shim.success();
     } catch (err) {
       return shim.error(err);
@@ -22,7 +36,14 @@ class DonationManager {
   }
 
   /**
-   * @dev Invoke functions of DonationManager chaincode
+   * Invoke functions of DonationManager chaincode
+   * Will be called whenever a chaincode setter or geeter functionality is called
+   *
+   * @async
+   * @function Invoke
+   * @param {Object} stub - encapsulates the APIs between the chaincode implementation and the Fabric peer
+   * @throws Will throw if the method being called raises an error
+   * @returns {Buffer} payload returned by the method being called
    */
   async Invoke(stub) {
     console.info("======== Donation Manager Invoke ========");
@@ -46,11 +67,20 @@ class DonationManager {
   }
 
   /**
-   * @dev Read donation using the transaction ID of the donation
+   * Read donation using the transaction ID of the donation
+   *
+   * @async
+   * @function readDonation
+   * @param {Object} stub - encapsulates the APIs between the chaincode implementation and the Fabric peer
+   * @param {Array} args - contains donation ID to query
+   * @throws Will throw if number of input arguments is not one
+   * @throws Will throw if if argument length is not 64
+   * @throws Will throw if state retrieval fails
+   * @returns {Buffer} donation object for a given donation ID
    */
   async readDonation(stub, args) {
-    Validations.checkArgsLength(args, 1);
-    Validations.equalsLength(args[0], 64);
+    Validations.checkLength(args, 1);
+    Validations.checkLength(args[0], 64);
 
     const donationId = args[0];
     console.info(`Query for Donation ID: ${donationId}`);
@@ -65,11 +95,20 @@ class DonationManager {
     };
     console.info("Query Response:");
     console.info(util.format(response));
-    return Helpers.toBuffer(response);
+    return Helpers.jsonToBuffer(response);
   }
 
   /**
-   * @dev Query multiple donation IDs
+   * Query multiple donation IDs
+   *
+   * @async
+   * @function readMultipleDonations
+   * @param {Object} stub - encapsulates the APIs between the chaincode implementation and the Fabric peer
+   * @param {Array} donationIds - contains donation IDs to query
+   * @throws Will throw if donationIds is not an Array
+   * @throws Will throw if donationId length is not 64
+   * @throws Will throw if state retrieval fails
+   * @returns {ArrayBuffer} Array of donation objects for given donation IDs
    */
   async readMultipleDonations(stub, donationIds) {
     Validations.isArray(donationIds);
@@ -80,7 +119,7 @@ class DonationManager {
     // Get the state from the ledger
     for (let i = 0; i < donationIds.length; i++) {
       const donationId = donationIds[i];
-      Validations.equalsLength(donationId, 64);
+      Validations.checkLength(donationId, 64);
       let donationData = await stub.getState(donationId);
       Validations.checkSuccessfulStateRetrieval(donationData);
       donationData = Helpers.defaultToUndefinedIfEmpty(donationData);
@@ -93,27 +132,43 @@ class DonationManager {
 
     console.info("Query Response:");
     console.info(util.format(response));
-    return Helpers.toBuffer(response);
+    return Helpers.jsonToBuffer(response);
   }
 
   /**
-   * @dev Check whether a donation exists in the state store
+   * Check whether a donation exists in the state store
+   *
+   * @async
+   * @function isPresent
+   * @param {Object} stub - encapsulates the APIs between the chaincode implementation and the Fabric peer
+   * @param {Array} args - contains donation ID to query
+   * @throws Will throw if number of input arguments is not one
+   * @throws Will throw if if argument length is not 64
+   * @returns {Buffer} Object containing bool values
    */
   async isPresent(stub, args) {
-    Validations.checkArgsLength(args, 1);
-    Validations.equalsLength(args[0], 64);
+    Validations.checkLength(args, 1);
+    Validations.checkLength(args[0], 64);
     const donationId = args[0];
     const donationData = await stub.getState(donationId);
     const response = { exists: !Helpers.isEmpty(donationData) };
-    return Helpers.toBuffer(response);
+    return Helpers.jsonToBuffer(response);
   }
 
   /**
-   * @dev get history of a given donation ID
+   * get history of a given donation ID
+   *
+   * @async
+   * @function getHistoryForDonation
+   * @param {Object} stub - encapsulates the APIs between the chaincode implementation and the Fabric peer
+   * @param {Array} args - contains donation ID to query
+   * @throws Will throw if number of input arguments is not one
+   * @throws Will throw if if argument length is not 64
+   * @returns {ArrayBuffer} Array of state update details for a given donation ID
    */
   async getHistoryForDonation(stub, args) {
-    Validations.checkArgsLength(args, 1);
-    Validations.equalsLength(args[0], 64);
+    Validations.checkLength(args, 1);
+    Validations.checkLength(args[0], 64);
     const donationId = args[0];
     console.info("Get history for Donation ID: %s\n", donationId);
 
@@ -124,11 +179,18 @@ class DonationManager {
   }
 
   /**
-   * @dev AddDonation function representing creation of a donation
+   * AddDonation function representing creation of a donation
    * Storing it against a unique transaction ID
+   *
+   * @async
+   * @function addDonation
+   * @param {Object} stub - encapsulates the APIs between the chaincode implementation and the Fabric peer
+   * @param {Array} donationData - contains project, itemType, amount in the same order
+   * @throws Will throw if number of input arguments is not three
+   * @returns {Buffer} Object containing the donation ID created
    */
   async addDonation(stub, donationData) {
-    Validations.checkArgsLength(donationData, 3);
+    Validations.checkLength(donationData, 3);
     const timestamp = stub.getTxTimestamp();
     const validity = Validations.checkDonationArgsType(donationData);
 
@@ -142,7 +204,7 @@ class DonationManager {
       await stub.putState(donationId, bufferedDonation);
 
       const result = { donationId };
-      return Helpers.toBuffer(result);
+      return Helpers.jsonToBuffer(result);
     } catch (error) {
       throw new Error(
         `Failed to create state for Donation ID: ${donationId}. Received ${error}`
@@ -151,11 +213,20 @@ class DonationManager {
   }
 
   /**
-   * @dev Update function representing update of a donation using Donation ID
+   * Update function representing update of a donation using Donation ID
+   *
+   * @async
+   * @function updateDonation
+   * @param {Object} stub - encapsulates the APIs between the chaincode implementation and the Fabric peer
+   * @param {Array} updatedDonationData - contains project, itemType, amount tags as well as the updated values. First all required tags are mentioned and then values are mentioned
+   * @throws Will throw if number of input arguments is not between 3 and 7
+   * @throws Will throw if if first argument length is not 64
+   * @throws Will throw if stae retrived for given donationID is empty
+   * @returns {Buffer} Object containing the donation ID updated and the transaction ID
    */
   async updateDonation(stub, updatedDonationData) {
     const donationId = updatedDonationData[0];
-    Validations.equalsLength(donationId, 64);
+    Validations.checkLength(donationId, 64);
     Validations.checkArgsLengthIsWithinRange(updatedDonationData, 3, 7);
     const updatedDonationJson = await Helpers.createUpdateJson(
       updatedDonationData.slice(1)
@@ -181,7 +252,7 @@ class DonationManager {
       const bufferedDonation = Helpers.jsonToBuffer(donationObj);
       await stub.putState(donationId, bufferedDonation);
       const result = { donationId, updateTx: await stub.getTxID() };
-      return Helpers.toBuffer(result);
+      return Helpers.jsonToBuffer(result);
     } catch (error) {
       throw new Error(
         `Failed to update state for Donation ID: ${donationId}. Received ${error}`
@@ -190,11 +261,20 @@ class DonationManager {
   }
 
   /**
-   * @dev Deletes a donation entity from state store
+   * Deletes a donation entity from state store
+   *
+   * @async
+   * @function removeDonation
+   * @param {Object} stub - encapsulates the APIs between the chaincode implementation and the Fabric peer
+   * @param {Array} donationData - contains donation ID to be removed
+   * @throws Will throw if number of input arguments is not one
+   * @throws Will throw if if first argument length is not 64
+   * @throws Will throw if stae retrived for given donationID is empty
+   * @returns {Buffer} Object containing the donation ID removed and the transaction ID
    */
   async removeDonation(stub, donationData) {
-    Validations.checkArgsLength(donationData, 1);
-    Validations.equalsLength(donationData[0], 64);
+    Validations.checkLength(donationData, 1);
+    Validations.checkLength(donationData[0], 64);
     const donationId = donationData[0];
 
     try {
@@ -205,7 +285,7 @@ class DonationManager {
       await stub.deleteState(donationId);
 
       const result = { donationId, deleteTx: await stub.getTxID() };
-      return Helpers.toBuffer(result);
+      return Helpers.jsonToBuffer(result);
     } catch (error) {
       throw new Error(
         `Failed to delete state for Donation ID: ${donationId}. Received ${error}`
