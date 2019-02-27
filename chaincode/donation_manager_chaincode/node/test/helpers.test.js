baf2d266d011a218f96c3c2d60df3a1cbbaeefee
chirag <chiragbfreelancer@gmail.com>
2019-02-27 16:19:39 +0530

    Added test cases for getAllResults and createUpdateJson

--- chaincode/donation_manager_chaincode/node/test/helpers.test.js
+++ chaincode/donation_manager_chaincode/node/test/helpers.test.js
@@ -1,92 +1,141 @@
 const assert = require("assert");
+const sinon = require("sinon");
+
 const helpers = require("../src/helpers");
-const testJson = { test: 'abc' }
+const { objCompare } = require("./testHelpers");
 
-describe("jsonToBuffer", function () {
+const testJson = { test: "abc" };
 
-  it("should return right buffer when pass an object", function () {
-    var result = helpers.jsonToBuffer(testJson);
-    result = result.toString('hex');
-    assert.equal(result, '7b2274657374223a22616263227d');
+describe("jsonToBuffer", () => {
+  it("should return right buffer when pass an object", () => {
+    let result = helpers.jsonToBuffer(testJson);
+    result = result.toString("hex");
+    assert.equal(result, "7b2274657374223a22616263227d");
   });
-
 });
 
-describe("bufferToJSON", function () {
-
-  it("should return right JSON when pass an buffer", function () {
-    var bufferValue = Buffer.from('7b2274657374223a22616263227d', 'hex')
-    var result = helpers.bufferToJSON(bufferValue);
-    result = helpers.objCompare(result, testJson)
+describe("bufferToJSON", () => {
+  it("should return right JSON when pass an buffer", () => {
+    let bufferValue = Buffer.from("7b2274657374223a22616263227d", "hex");
+    let result = helpers.bufferToJSON(bufferValue);
+    result = objCompare(result, testJson);
     assert.equal(result, true);
   });
 
-  it("should return right JSON when pass an buffer", function () {
+  it("should return right JSON when a buffer is passed", () => {
     try {
-      var bufferValue = Buffer.from('', 'hex')
-      var result = helpers.bufferToJSON(bufferValue);
-      result = helpers.objCompare(result, testJson)
+      let bufferValue = Buffer.from("", "hex");
+      let result = helpers.bufferToJSON(bufferValue);
+      result = objCompare(result, testJson);
       assert.equal(result, true);
     } catch (error) {
-      console.log(error.message);
-      // assert.equal(error.message, "Donation data is absent OR no state registered.");
+      assert.equal(error.message, "Error parsing value to JSON: .");
     }
-    
   });
-
 });
 
-describe("bufferToJSON", function () {
-
-  it("should return if string is empty or not", function () {
-    var result = helpers.isEmpty("");
+describe("bufferToJSON", () => {
+  it("should return if string is empty or not", () => {
+    let result = helpers.isEmpty("");
     assert.equal(result, true);
   });
-
 });
 
-describe("defaultToUndefinedIfEmpty", function () {
-
-  it("should return undefined when pass an empty object", function () {
-    var result = helpers.defaultToUndefinedIfEmpty("");
-    result = result.toString('hex');
-    assert.equal(result, '756e646566696e6564');
+describe("defaultToUndefinedIfEmpty", () => {
+  it("should return undefined when pass an empty object", () => {
+    let result = helpers.defaultToUndefinedIfEmpty("");
+    result = result.toString("hex");
+    assert.equal(result, "756e646566696e6564");
   });
-
 });
 
-describe("formatToJson", function () {
-
-  it("should return object when pass an array of values", function () {
+describe("formatToJson", () => {
+  it("should return object when pass an array of values", () => {
     const expectedJson = {
-      project: 'test',
-      itemType: 'test',
+      project: "test",
+      itemType: "test",
       amount: 3.2,
-      timestamp: 'test',
-      validity: 'test'
+      timestamp: "test",
+      validity: "test"
+    };
+    let result = helpers.formatToJson(["test", "test", "3.2", "test", "test"]);
+    result = objCompare(result, expectedJson);
+    assert.equal(result, true);
+  });
+});
+
+describe("createUpdateJson", () => {
+  it("should return right updated object when pass an array", async () => {
+    const expectedJson = { testValue1: "testValue2" };
+    let result = await helpers.createUpdateJson(["testValue1", "testValue2"]);
+    result = objCompare(result, expectedJson);
+    assert.equal(result, true);
+  });
+
+  it("should fail if number of elements in array is odd", async () => {
+    try {
+      let result = await helpers.createUpdateJson([
+        "testValue1",
+        "testValue2",
+        "testvalue3"
+      ]);
+      assert.fail("should have thrown before");
+    } catch (error) {
+      assert.equal(
+        error.message,
+        "Number of elements in update request should be even"
+      );
     }
-    var result = helpers.formatToJson(['test', 'test', '3.2', 'test', 'test']);
-    result = helpers.objCompare(result, expectedJson)
-    assert.equal(result, true);
   });
-
 });
 
-describe("createUpdateJson", function () {
+describe("getAllResults", () => {
+  it("should return all results in JSON when pass an iterator and history", async () => {
+    const iterator = {
+      next: () => {},
+      close: () => {}
+    };
+    const expectedResult = {
+      value: {
+        tx_id:
+          "fc7980500c5b1acbfa0754d30e7c988e3a28829e8d8bfdbbbf1127a23d8fd479",
+        value: Buffer.from(
+          '{"project":"ITU","itemType":"grains","amount":"1","timestamp":{"seconds":{"low":1551256489,"high":0,"unsigned":false},"nanos":949499015},"validity":true}'
+        ),
+        timestamp: {
+          seconds: { low: 1551188076, high: 0, unsigned: false },
+          nanos: 494418844
+        },
+        is_delete: false
+      },
+      done: false
+    };
 
-  it("should return right updated object when pass an array", async function () {
-    const expectedJson = { testValue1: 'testValue2' }
-    var result = await helpers.createUpdateJson(['testValue1', 'testValue2']);
-    result = helpers.objCompare(result, expectedJson)
-    assert.equal(result, true);
+    stubFunctionNext = sinon.stub(iterator, "next");
+    stubFunctionClose = sinon.stub(iterator, "close");
+
+    stubFunctionNext.onFirstCall().returns(expectedResult);
+    stubFunctionNext.onSecondCall().returns({ done: true });
+
+    const result = await helpers.getAllResults(iterator);
+    objCompare(
+      result[0].Record,
+      helpers.bufferToJSON(expectedResult.value.value)
+    );
   });
 
+  it("should return empty array if iterator returns done on first next", async () => {
+    const iterator = {
+      next: () => {},
+      close: () => {}
+    };
+
+    stubFunctionNext = sinon.stub(iterator, "next");
+    stubFunctionClose = sinon.stub(iterator, "close");
+
+    stubFunctionNext.returns({ done: true });
+
+    const result = await helpers.getAllResults(iterator);
+    objCompare(result, []);
+  });
 });
-
-// describe("getAllResults", function () {
-//   // it("should return all results in JSON when pass an iterator and history", function () {
-//   //   var result = helpers.getAllResults({"test": "abc"});
-//   //   assert.equal(status, 200);
-//   // });
-
-// });
